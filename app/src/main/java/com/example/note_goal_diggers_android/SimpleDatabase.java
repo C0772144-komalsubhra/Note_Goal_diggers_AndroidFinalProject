84d4a9e869a789650d8043f2b96e2d49c4f2e01a
gagan <sherry2312.gk@gmail.com>
2020-07-18 01:36:28 -0400

    udating table code added

--- app/src/main/java/com/example/note_goal_diggers_android/SimpleDatabase.java
+++ app/src/main/java/com/example/note_goal_diggers_android/SimpleDatabase.java
@@ -40,7 +40,11 @@ public class SimpleDatabase extends SQLiteOpenHelper {
 
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if(oldVersion >= newVersion)
+            return;
 
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
     }
 }
