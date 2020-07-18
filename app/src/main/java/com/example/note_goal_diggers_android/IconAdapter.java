040960398f829e73ce552e4528cbe390e5bccba5
gagan <sherry2312.gk@gmail.com>
2020-07-18 15:38:03 -0400

    implementing base adapter

--- app/src/main/java/com/example/note_goal_diggers_android/IconAdapter.java
+++ app/src/main/java/com/example/note_goal_diggers_android/IconAdapter.java
@@ -1,4 +1,27 @@
 package com.example.note_goal_diggers_android;
 
-public class IconAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class IconAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
 }
