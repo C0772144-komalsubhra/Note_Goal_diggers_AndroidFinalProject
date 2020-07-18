4fa7eec0e23d5ebcf828dced3ff233945e209613
gagan <sherry2312.gk@gmail.com>
2020-07-18 15:08:35 -0400

    maps activity added

--- app/src/main/java/com/example/note_goal_diggers_android/MainActivity.java
+++ app/src/main/java/com/example/note_goal_diggers_android/MainActivity.java
@@ -1,21 +1,27 @@
 package com.example.note_goal_diggers_android;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -53,5 +59,84 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,categoryName);
+        listView.setAdapter(adapter);
+
+
+
+
+        addCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+
+                View view = inflater.inflate(R.layout.category_dialog,null);
+
+                builder.setView(view);
+
+                final EditText editTextCategory = view.findViewById(R.id.edit_text_cat);
+
+
+                builder.setPositiveButton("ADD", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+
+
+                        String cat = editTextCategory.getText().toString();
+
+                        categoryName.add(cat);
+
+                        try {
+                            sharedPreferences.edit().putString("cname",ObjectSerializer.serialize(categoryName)).apply();
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+//                            if(dataBaseHelper.addNote(cat," hb"," hvbh"," hbvh")){
+//                                Toast.makeText(MainActivity.this, "added", Toast.LENGTH_SHORT).show();
+//                            }else {
+//                                Toast.makeText(MainActivity.this, "not added", Toast.LENGTH_SHORT).show();
+//                            }
+
+
+
+                        listView.setAdapter(adapter);
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                catPosition = position;
+                Intent intent = new Intent(MainActivity.this, NotesActivity.class);
+                intent.putExtra("id", position);
+                startActivity(intent);
+
+            }
+        });
+
+
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/note_goal_diggers_android/NotesActivity.java
@@ -0,0 +1,14 @@
+package com.example.note_goal_diggers_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+    }
+}
