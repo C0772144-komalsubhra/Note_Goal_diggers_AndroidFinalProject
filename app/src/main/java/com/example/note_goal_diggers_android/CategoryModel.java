4c435d2a1673236279a207b7ce4508e59ae35ff4
gagan <sherry2312.gk@gmail.com>
2020-07-18 15:34:25 -0400

    constructor generated

--- app/src/main/java/com/example/note_goal_diggers_android/CategoryModel.java
+++ app/src/main/java/com/example/note_goal_diggers_android/CategoryModel.java
@@ -3,4 +3,21 @@ package com.example.note_goal_diggers_android;
 import java.io.Serializable;
 
 public class CategoryModel  implements Serializable {
+    int id;
+
+    String title,description,date;
+    double noteLat,noteLong;
+    String audio;
+    String image;
+
+    public CategoryModel(int id, String title, String description, String date, double noteLat, double noteLong, String audio, String image) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.noteLat = noteLat;
+        this.noteLong = noteLong;
+        this.audio = audio;
+        this.image = image;
+    }
 }
