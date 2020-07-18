da97f59f37d6b5721ab586235185a25fd4a2d0e0
gagan <sherry2312.gk@gmail.com>
2020-07-18 02:39:46 -0400

    pushedall

--- app/src/main/java/com/example/note_goal_diggers_android/Edit.java
+++ app/src/main/java/com/example/note_goal_diggers_android/Edit.java
@@ -1,4 +1,25 @@
 package com.example.note_goal_diggers_android;
 
-public class Edit {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+public class Edit extends AppCompatActivity {
+    Toolbar toolbar;
+    EditText nTitle,nContent;
+    Calendar c;
+    String todaysDate;
+    String currentTime;
+    long nId;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
 }
