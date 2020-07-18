febc3506c11eae66690eb72b7dc2069722971048
gagan <sherry2312.gk@gmail.com>
2020-07-18 14:42:25 -0400

    adding button

--- app/src/main/java/com/example/note_goal_diggers_android/MainActivity.java
+++ app/src/main/java/com/example/note_goal_diggers_android/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.note_goal_diggers_android;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -12,6 +13,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Adapter;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,9 +21,17 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Button addCategory;
+    SimpleDatabase dataBaseHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Categories");
+
+
+       
+        addCategory = findViewById(R.id.btn_add_category);
     }}
