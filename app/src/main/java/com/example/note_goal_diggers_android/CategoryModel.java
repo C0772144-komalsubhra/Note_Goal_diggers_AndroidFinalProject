305e9f5514a20fda1c378a09e0a58065a38f34d6
gagan <sherry2312.gk@gmail.com>
2020-07-18 15:35:00 -0400

    generated getter and setter

--- app/src/main/java/com/example/note_goal_diggers_android/CategoryModel.java
+++ app/src/main/java/com/example/note_goal_diggers_android/CategoryModel.java
@@ -10,6 +10,70 @@ public class CategoryModel  implements Serializable {
     String audio;
     String image;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getNoteLat() {
+        return noteLat;
+    }
+
+    public void setNoteLat(double noteLat) {
+        this.noteLat = noteLat;
+    }
+
+    public double getNoteLong() {
+        return noteLong;
+    }
+
+    public void setNoteLong(double noteLong) {
+        this.noteLong = noteLong;
+    }
+
+    public String getAudio() {
+        return audio;
+    }
+
+    public void setAudio(String audio) {
+        this.audio = audio;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
     public CategoryModel(int id, String title, String description, String date, double noteLat, double noteLong, String audio, String image) {
         this.id = id;
         this.title = title;
