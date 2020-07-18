cb6dd9f912f7a4c923e3152f7598bffc25ba9a51
gagan <sherry2312.gk@gmail.com>
2020-07-18 15:39:16 -0400

    declaring variables

--- app/src/main/java/com/example/note_goal_diggers_android/IconAdapter.java
+++ app/src/main/java/com/example/note_goal_diggers_android/IconAdapter.java
@@ -1,10 +1,23 @@
 package com.example.note_goal_diggers_android;
 
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import java.util.List;
+
 public class IconAdapter extends BaseAdapter {
+
+    Context context;
+   
+    public int position;
+    String photopath;
+    Bitmap photobitmap;
+
+    List<CategoryModel> categoryModelList;
+
     @Override
     public int getCount() {
         return 0;
