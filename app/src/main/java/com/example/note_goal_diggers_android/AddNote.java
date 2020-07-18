85819a8f1470b310faaaad68c1ebe6886d84fb68
gagan <sherry2312.gk@gmail.com>
2020-07-18 02:21:58 -0400

    declare vaiables

--- app/src/main/java/com/example/note_goal_diggers_android/AddNote.java
+++ app/src/main/java/com/example/note_goal_diggers_android/AddNote.java
@@ -3,9 +3,18 @@ package com.example.note_goal_diggers_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toolbar;
+
+import java.util.Calendar;
 
 public class AddNote extends AppCompatActivity {
 
+    Toolbar toolbar;
+    EditText noteTitle,noteDetails;
+    Calendar c;
+    String todaysDate;
+    String currentTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
