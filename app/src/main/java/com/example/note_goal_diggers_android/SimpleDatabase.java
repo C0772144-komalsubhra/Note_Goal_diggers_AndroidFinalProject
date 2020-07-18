2de0e02079d85aee3c220ac959afb3add5f1aa6a
gagan <sherry2312.gk@gmail.com>
2020-07-18 01:30:53 -0400

    implementing SQllite and declaring vribles

--- app/src/main/java/com/example/note_goal_diggers_android/SimpleDatabase.java
+++ app/src/main/java/com/example/note_goal_diggers_android/SimpleDatabase.java
@@ -1,4 +1,29 @@
 package com.example.note_goal_diggers_android;
 
-public class SimpleDatabase {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SimpleDatabase extends SQLiteOpenHelper {
+
+
+    // declare require values
+    private static final int DATABASE_VERSION = 2;
+    private static final String DATABASE_NAME = "SimpleDB";
+    private static final String TABLE_NAME = "SimpleTable";
+
+    public SimpleDatabase(Context context)
+    {
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
