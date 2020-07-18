541a832a704495fc3aca2b050975a681359b8e36
Komal Subhra <60164715+C0772144-komalsubhra@users.noreply.github.com>
2020-07-18 19:22:22 -0400

    Update NoteClass.java

--- app/src/main/java/com/example/note_goal_diggers_android/NoteClass.java
+++ app/src/main/java/com/example/note_goal_diggers_android/NoteClass.java
@@ -2,7 +2,7 @@ package com.example.note_goal_diggers_android;
 
 import java.io.Serializable;
 
-public class CategoryModel  implements Serializable {
+public class NoteClass  implements Serializable {
     int id;
 
     String title,description,date;
