acb00b993b8c85f61946bf360dab1400570cb426
gagan <sherry2312.gk@gmail.com>
2020-07-18 14:39:57 -0400

    chganging the layout of add category

--- app/src/main/java/com/example/note_goal_diggers_android/MainActivity.java
+++ app/src/main/java/com/example/note_goal_diggers_android/MainActivity.java
@@ -19,69 +19,9 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-  Toolbar toolbar;
-    RecyclerView recyclerView;
 
- RecyclerView.Adapter adapter;
-    TextView noItemText;
-    SimpleDatabase simpleDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
-        setSupportActionBar(toolbar);
-        noItemText = findViewById(R.id.noItemText);
-        simpleDatabase = new SimpleDatabase(this);
-        List<Note> allNotes = simpleDatabase.getAllNotes();
-        recyclerView = findViewById(R.id.allNotesList);
-
-        if(allNotes.isEmpty()){
-            noItemText.setVisibility(View.VISIBLE);
-        }else {
-            noItemText.setVisibility(View.GONE);
-            displayList(allNotes);
-        }
-
-    }
-
-
-
-    private void displayList(List<Note> allNotes) {
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new Adapter(this,allNotes);
-        recyclerView.setAdapter(adapter);
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu,menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.add){
-            Toast.makeText(this, "Add New Note", Toast.LENGTH_SHORT).show();
-            Intent i = new Intent(this,AddNote.class);
-            startActivity(i);
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        List<Note> getAllNotes = simpleDatabase.getAllNotes();
-        if(getAllNotes.isEmpty()){
-            noItemText.setVisibility(View.VISIBLE);
-        }else {
-            noItemText.setVisibility(View.GONE);
-            displayList(getAllNotes);
-        }
-
-
-    }
-
-}
+    }}
